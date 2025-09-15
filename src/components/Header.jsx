6362cd9f7f9e5716ce3e00da5c3bb8f843ebbb08
Sheena <sheenaseciban.17@gmail.com>
2025-09-15 11:32:17 +0800

    Initial search button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,27 @@ function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="flex flex-row items-center justify-between sm:justify-around p-2">
+    <header className="flex flex-row items-center justify-between sm:justify-around p-2 text-sm text-[#111155]">
       
       <nav className="hidden sm:flex justify-between items-center gap-3">
         <img className="rounded-full w-0.4 h-0.4" src="../src/assets/images/initial-movie-logo.png" alt="Movie Quest"/>
-        <p className="font-main-header"> Movie Quest </p>
+        <p className="font-main-header tracking-wide"> Movie Quest </p>
       </nav>
+      
+      <form class="max-w-md mx-auto">   
+          <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                  </svg>
+              </div>
+              <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for movies and more" required />
+              <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+      </form>
 
-      <nav className="hidden sm:flex justify-between items-center gap-4 font-side-header">
+      <nav className="hidden sm:flex justify-between items-center gap-8 font-side-header font-semibold">
         <a href="#" className="hover:text-gray-500">
           Login
         </a>
@@ -22,7 +35,7 @@ function Header() {
         </a>
       </nav>
 
-      <nav className="sm:hidden flex flex-col items-end gap-1">
+      <nav className="sm:hidden flex flex-col items-end gap-1 font-side-header font-semibold">
         <button
           onClick={() => setShowMenu(!showMenu)}
           className="sm:hidden font-bold text-xl hover:text-gray-500"
